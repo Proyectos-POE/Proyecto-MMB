2280e559132a097d73af4a5f39038a41deac4bb4
Nicolas Herrera <herrera.nicolas@correounivalle.edu.co>
2022-10-30 15:34:08 -0500

    Carga Inicial

---
+++ src/proyecto/memorabble/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.memorabble;
+
+/**
+ *
+ * @author Nicolas Herrera <herrera.nicolas@correounivalle.edu.co>
+ * @author Samuel Galindo Cuevas <samuel.galindo@correounivalle.edu.co>
+ * @author Yenny Rivas Tello <yenny.rivas@correounivalle.edu.co>
+ */
+public class main 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }  
+}
